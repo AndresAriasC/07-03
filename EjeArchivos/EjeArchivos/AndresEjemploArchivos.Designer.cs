7a307374fc3b20e2f11e36d1de9880632087a2cc
AndresAriasC <110309134+AndresAriasC@users.noreply.github.com>
2023-03-07 10:27:24 -0600

    Add files via upload

---
+++ EjeArchivos/EjeArchivos/AndresEjemploArchivos.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace EjeArchivos
+{
+    partial class AndresEjemploArchivos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.txtSalida = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introduzca el archivo o directorio";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(34, 66);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(292, 23);
+            this.txtEntrada.TabIndex = 1;
+            this.txtEntrada.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEntrada_KeyDown);
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.Location = new System.Drawing.Point(34, 109);
+            this.txtSalida.Multiline = true;
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(292, 267);
+            this.txtSalida.TabIndex = 2;
+            // 
+            // AndresEjemploArchivos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 409);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label1);
+            this.Name = "AndresEjemploArchivos";
+            this.Text = "Ejemplo Archivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtEntrada;
+        private TextBox txtSalida;
+    }
+}
---
+++ EjeArchivos/EjeArchivos/AndresEjemploArchivos.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjeArchivos
+{
+    public partial class AndresEjemploArchivos : Form
+    {
+        public AndresEjemploArchivos()
+        {
+            InitializeComponent();
+        }
+
+        //se invoca cuando el osuario oprime una tecla
+        private void txtEntrada_KeyDown(object sender, KeyEventArgs e)
+        {
+            //determina si el usuario opimio la tecla enter
+
+            if (e.KeyCode == Keys.Enter) {
+                string name; // nombre del archivo
+
+                //obtiene el archivo o directorio especifico por el usuario
+
+                name = txtEntrada.Text;
+
+                //determina si name es un archivo
+                if (File.Exists(name))
+                {
+                    //obtiene la dfecha de creacion del archivo
+                    // su fecha de modificaciont etc
+                    txtSalida.Text = obtenerInformacion(name);
+
+
+                    try
+                    {
+                        //OBTIENE LECTOR Y CONTENIDO DEL ARCHIVO
+                        StreamReader sr = new StreamReader(name);
+                        txtSalida.Text += sr.ReadToEnd();
+                    }
+                    //maneja exception si StreamReader no esta disponible
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error al leer el archivo", "Error de archivo", 
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+
+                }
+                //determina si name es un directorio
+                else if (Directory.Exists(name))
+                {
+                    string[] listaDirectorio; //arreglo para los directorios
+                                              //obtiene la dfecha de creacion del archivo
+                                              // su fecha de modificaciont etc
+                    txtSalida.Text = obtenerInformacion(name);
+                    //obtener la lista de los archivos/directorios del directorio especificado
+                    listaDirectorio = Directory.GetDirectories(name);
+
+                    txtSalida.Text += "\r\n\r\nContenido del directorio: \r\n";
+                    //imprime en pantalla el contenido de la listaDirectorios
+
+                    for (int i = 0; i < listaDirectorio.Length; i++)
+                    {
+                        txtSalida.Text += listaDirectorio[i] + "\r\n";
+                    }
+
+                }
+                else {
+                    //notific al usuario que no existe el directorio del archivo
+                    MessageBox.Show(txtEntrada.Text + " no existe", "Error de archivo",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+
+            }
+
+
+        }
+        //obtiene informacion sobre el archivo o directorio
+        private string obtenerInformacion(string name)
+        {
+            String informacion;
+
+            // imprime mensaje indicando que existe el archivo o directorio
+            informacion = name + " Existe\r\n\r\n";
+
+            //imprime en pantalla la fecha de modificacion del archivo o directorio
+            informacion += "Modificacion ultima: " + File.GetLastWriteTime(name) + "\r\n";
+
+            //imprime en pantalla la fecha y hora de creacion del archivo o directorio
+            informacion += "Creacion: " + File.GetCreationTime(name) + "\r\n";
+
+            //imprime en pantalla el ultimo acceso del archivo o directorio
+            informacion += "Ultimo acceso: " + File.GetLastAccessTime(name) + "\r\n";
+
+            return informacion;
+
+
+        }
+    }
+}
